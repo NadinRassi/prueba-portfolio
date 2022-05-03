d8eb24819704a69afb0834b353f6c00c7a67b7d0
Nadin Rassi <95599571+NadinRassi@users.noreply.github.com>
2022-05-03 13:42:09 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutModule } from './@components/about/about.module';
+import { ContactModule } from './@components/contact/contact.module';
+import { CopyrightModule } from './@components/copyright/copyright.module';
+import { FootersModule } from './@components/footers/footers.module';
+import { HeaderModule } from './@components/header/header.module';
+import { NavbarModule } from './@components/navbar/navbar.module';
+import { PortfolioModule } from './@components/portfolio/portfolio.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AboutModule,
+    ContactModule,
+    CopyrightModule,
+    FootersModule,
+    HeaderModule,
+    NavbarModule,
+    PortfolioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
